e811c78ee911d0df96f616ce3f869cf5cb2958f2
Mohammad-imran75 <imranbd7531@gmail.com>
2024-02-14 17:04:43 +0600

    added the rhombus area in main section

--- scripts/geometry.js
+++ scripts/geometry.js
@@ -39,3 +39,50 @@ function calculateRectangleArea(){
    const areaSpan = document.getElementById("rectangle-area");
    areaSpan.innerText = area;
 }
+
+// parallelogram area calculate 
+
+function calculateParallelogramArea(){
+    // get the parallelogram base
+    const  parallelogramBaseInput = document.getElementById('parallelogram-base');
+    const parallelogramBaseText = parallelogramBaseInput.value;
+    const base = parseFloat(parallelogramBaseText);
+    console.log(base);
+
+    // get the parallelogram height 
+    const parallelogramHeightInput = document.getElementById('parallelogram-height');
+    const parallelogramHeightText = parallelogramHeightInput.value;
+    const height = parseFloat(parallelogramHeightText);
+    console.log(height);
+
+    // get the parallelogram area 
+    const area = base * height;
+    console.log('The are of parallelogram :' , area );
+
+    //display showing the parallelogram area
+
+    const areaSpan = document.getElementById('parallelogram-area');
+    areaSpan.innerText = area;
+}
+
+// Rhombus area calculate 
+function calculateRhombusArea(){
+    // get the rhombus first karna
+    const rhombusFirstKarnaInput = document.getElementById('first-karna');
+    const rhombusFirstKarnaText = rhombusFirstKarnaInput.value;
+    const firstKarna =parseFloat(rhombusFirstKarnaText);
+    console.log(firstKarna);
+    // get the rhombus second karna
+
+    const rhombusSecondKarnaInput = document.getElementById('second-karna');
+    const rhombusSecondKarnaText = rhombusSecondKarnaInput.value;
+    const secondKarna = parseFloat(rhombusSecondKarnaText);
+    console.log(secondKarna);
+
+    // area of the rhombus 
+    const area = 0.5 * firstKarna * secondKarna;
+    console.log("The area of Rhombus :", area);
+    // display showing the rhombus area
+    const areaSpan = document.getElementById('rhombus-area');
+    areaSpan.innerText = area;
+}
