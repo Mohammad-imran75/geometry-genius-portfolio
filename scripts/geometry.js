bfc63b99da46040567fb7afcc9aa992770dde231
Mohammad-imran75 <imranbd7531@gmail.com>
2024-02-14 16:13:33 +0600

    added the rectangle card in main section

--- scripts/geometry.js
+++ scripts/geometry.js
@@ -15,3 +15,27 @@ function getTriangle(){
     const triangleAreaSpan = document.getElementById('triangle-area');
     triangleAreaSpan.innerText = area; 
 }
+
+// Rectangle Area calculate
+
+function calculateRectangleArea(){
+    // get the rectangle length 
+    const rectangleWidthInput = document.getElementById("rectangle-width");
+    const rectangleWidthText =rectangleWidthInput.value;
+    const width =parseFloat(rectangleWidthText);
+    console.log(width);
+    
+    // get the rectangle length
+    const rectangleLengthInput =document.getElementById('rectangle-length');
+    const rectangleLengthText = rectangleLengthInput.value;
+    const length = parseFloat(rectangleLengthText);
+    console.log(length);
+
+    // area of the rectangle 
+    const area = width * length;
+    console.log("Area of the rectangle :", area);
+
+   // display showing the rectangle area
+   const areaSpan = document.getElementById("rectangle-area");
+   areaSpan.innerText = area;
+}
